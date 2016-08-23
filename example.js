ea0cc11594ae7cf023007f4d09fa5792573ff463
Julian Gruber <julian@juliangruber.com>
2016-08-23 19:14:39 +0200

    use more solid approach for spawning chrome

--- example.js
+++ example.js
@@ -1,8 +1,7 @@
 const chrome = require('.')
 
 chrome({
-  uri: 'https://github.com/',
-  hide: !!process.argv[2]
+  uri: 'https://github.com/'
 }, (err, ps) => {
   if (err) throw err    
   ps.on('error', console.error)
--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 const spawn = require('child_process').spawn
+const find = require('osx-find-executable')
+const foreground = require('osx-foreground')
 
 module.exports = (opts, cb) => {
   const env = Object.assign({}, process.env)
   if (opts.display) env.DISPLAY = `:${opts.display}`
 
-  const args = [
-    '--wait-apps',
-    '--new',
-    '--fresh',
-    '-b', 'com.google.Chrome',
-    opts.hide && '--hide',
-    '--args',
-    opts.proxyServer && `--proxy-server=${opts.proxyServer}`,
-    `--user-data-dir=${opts.dataDir || `/tmp/${Math.random().toString(16).slice(2)}`}`,
-    opts.dataDir && `--user-data-dir=${opts.dataDir}`,
-    '--disable-restore-session-state',
-    '--no-default-browser-check',
-    '--start-maximized',
-    '--disable-default-apps',
-    '--disable-sync',
-    '--enable-fixed-layout',
-    '--no-first-run',
-    '--noerrdialogs',
-    opts.uri
-  ].filter(Boolean)
+  find('com.google.Chrome', (err, exec) => {
+    if (err) return cb(err)
 
-  cb(null, spawn('open', args, { env }))
+    const args = [
+      opts.proxyServer && `--proxy-server=${opts.proxyServer}`,
+      `--user-data-dir=${opts.dataDir || `/tmp/${Math.random().toString(16).slice(2)}`}`,
+      opts.dataDir && `--user-data-dir=${opts.dataDir}`,
+      '--disable-restore-session-state',
+      '--no-default-browser-check',
+      '--start-maximized',
+      '--disable-default-apps',
+      '--disable-sync',
+      '--enable-fixed-layout',
+      '--no-first-run',
+      '--noerrdialogs',
+      opts.uri
+    ].filter(Boolean)
+
+    const ps = spawn(exec, args, { env })
+    if (opts.background) return cb(null, ps)
+
+    foreground(ps.pid, err => {
+      if (err) return cb(err)
+      cb(null, ps)
+    })
+  })
 }
